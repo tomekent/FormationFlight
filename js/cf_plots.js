ee93179b534f21cee8f3c4e35b55e26303390211
Tom Kent <tomekent@hotmail.com>
2020-01-23 14:18:07 +0000

    Combined functionality for single html with map variable, added co2 and fuel kg

--- js/cf_plots.js
+++ js/cf_plots.js
@@ -3,7 +3,7 @@
 
 /*
 function setData(t) {
-	var fname = "Data/Form_data_t_"+t+".csv";	
+	var fname = "Data/Form_data_t_"+t+".csv";
 }
 */
 function init() {
@@ -15,175 +15,221 @@ function init() {
 inputVars = getUrlVars()["fname"];
 
 if (typeof inputVars != 'undefined') {
-	fname = "Data/" + getUrlVars()["fname"] +".csv"
+	fname = "Data/" + getUrlVars()["fname"] +".csv";
 	}
-	else 
+	else
 	{
 /* 	window.alert("No input data: Using default"); */
-	fname = "Data/EasyJet/Form_data_t_15.csv";	
+	fname = "Data/EasyJet/Form_data_t_15.csv";
+}
+
+
+inputVars = getUrlVars()["map"];
+
+var plotmap;
+var testvar;
+if (typeof inputVars != 'undefined') {
+    // plotmap = getUrlVars()["map"];
+    if (getUrlVars()["map"] == 'True'){
+        var plotmap  = Boolean(true);
+        console.log('plot map true')
+    }else{
+        var plotmap = Boolean(false);
+        console.log('plot map false')
+    }
+
+	}
+	else
+	{
+/* 	window.alert("No input data: Using default"); */
+	plotmap = false;
 }
 
 
 function init() {d3.csv(fname, function(error, flights) {
-/*
-function init() {d3.csv(fname, function(error, flights) {
-*/
-
-var map;
-var markers = [];
-var idDimension;
-var idGrouping;
-
-initMap();
-  // Various formatters.
-  var formatNumber = d3.format(",d"),
-      formatChange = d3.format("+,d"),
+    var idDimension;
+    var idGrouping;
+    if (plotmap == true) {
+        var map;
+        var markers = [];
+        initMap();
+        console.log('map initiated')
+    }else{
+        console.log('not plotting map')
+    }
+    console.log(testvar)
+// Various formatters.
+var formatNumber = d3.format(",d"),
+  formatChange = d3.format("+,d"),
 /*       formatDate = d3.time.format("%B %d, %Y"), */
-      formatDate = d3.time.format("%a"),
-      formatTime = d3.time.format("%I:%M %p"),
-      formatPC = d3.format(",.1%");
-      formatNo = d3.format(",.0f");
+  formatDate = d3.time.format("%a"),
+  formatTime = d3.time.format("%I:%M %p"),
+  formatPC = d3.format(",.1%");
+  formatNo = d3.format(",.0f");
 
-  // A nest operator, for grouping the flight list.
-  var nestByDate = d3.nest()
-      .key(function(d) { return d3.time.day(d.date); });
-  var nestByPC = d3.nest()
-      .key(function(d) { return FB_pc; });
+// A nest operator, for grouping the flight list.
+var nestByDate = d3.nest()
+  .key(function(d) { return d3.time.day(d.date); });
+var nestByPC = d3.nest()
+  .key(function(d) { return FB_pc; });
 
-  // A little coercion, since the CSV is untyped.
-  flights.forEach(function(d, i) {
-	    d.index = i;
-		d.TA = parseDate2(d.TA,d.day),
-		d.TB = parseDate2(d.TB,d.day),
-		d.tdiff = +d.tdiff,
-		d.Dept_A = d.Dept_A,
-		d.Dept_B = d.Dept_B,
-		d.Dest_A = d.Dest_A,
-		d.Dest_B = d.Dest_B,
-		d.FB_pc = +d.FB_pc,
-		d.cruise_pc = +d.cruise_pc,
-		d.Distance = +d.Distance,
-		d.dev = +d.dev,
-		d.FB_solo = +d.FB_solo,
-		d.FB_form = +d.FB_form,
-		d.lon1 = +d.lon1,
-		d.lat1 = +d.lat1,
-		d.lon2 = +d.lon2,
-		d.lat2 = +d.lat2,
-		d.lon3 = +d.lon3,
-		d.lat3 = +d.lat3,
-		d.lon4 = +d.lon4,
-		d.lat4 = +d.lat4,
-		d.lon5 = +d.lon5,
-		d.lat5 = +d.lat5,
-		d.lon6 = +d.lon6,
-		d.lat6 = +d.lat6
-  });
+// A little coercion, since the CSV is untyped.
+flights.forEach(function(d, i) {
+    d.index = i;
+	d.TA = parseDate2(d.TA,d.day),
+	d.TB = parseDate2(d.TB,d.day),
+	d.tdiff = +d.tdiff,
+	d.Dept_A = d.Dept_A,
+	d.Dept_B = d.Dept_B,
+	d.Dest_A = d.Dest_A,
+	d.Dest_B = d.Dest_B,
+	d.FB_pc = +d.FB_pc,
+	d.cruise_pc = +d.cruise_pc,
+	d.Distance = +d.Distance,
+	d.dev = +d.dev,
+	d.FB_solo = +d.FB_solo,
+	d.FB_form = +d.FB_form,
+    d.FB_kg = +d.FB_solo - d.FB_form,
+    d.FB_tonne = + d.FB_kg/1000,
+    d.FB_co2 = +(d.FB_kg * 3.16),
+    d.FB_co2_tonne = +d.FB_co2/1000,
+	d.lon1 = +d.lon1,
+	d.lat1 = +d.lat1,
+	d.lon2 = +d.lon2,
+	d.lat2 = +d.lat2,
+	d.lon3 = +d.lon3,
+	d.lat3 = +d.lat3,
+	d.lon4 = +d.lon4,
+	d.lat4 = +d.lat4,
+	d.lon5 = +d.lon5,
+	d.lat5 = +d.lat5,
+	d.lon6 = +d.lon6,
+	d.lat6 = +d.lat6
+});
 
-  // Create the crossfilter for the relevant dimensions and groups.
-  var flight = crossfilter(flights),
-      all = flight.groupAll(),
-      day = flight.dimension(function(d) { return d.TA; }),
-      
-	  date = flight.dimension(function(d) { return d.TA.getDate(); }),
-      dates = day.group(d3.time.day),
+// Create the crossfilter for the relevant dimensions and groups.
+var flight = crossfilter(flights),
+  all = flight.groupAll(),
+  day = flight.dimension(function(d) { return d.TA; }),
 
-      hour = flight.dimension(function(d) { return d.TA.getHours(); }),
-      hours = hour.group(function(d) {return Math.floor(d);}),
-      
-      cruise_pc = flight.dimension(function(d) { return d.cruise_pc; }),
-      cruise_pcs = cruise_pc.group(function(d) { return Math.floor(d * 10) / 10;}),
+  date = flight.dimension(function(d) { return d.TA.getDate(); }),
+  dates = day.group(d3.time.day),
 
-      FB_pc = flight.dimension(function(d) { return d.FB_pc; }),
-      FB_pcs = FB_pc.group(function(d) { return Math.floor(d * 10) / 10;}),
-      
-      distance = flight.dimension(function(d) { return d.Distance; }),
-      distances = distance.group(function(d) { return Math.floor(d / 50) * 50; }),
-      
-      time_allowed = flight.dimension(function(d) { return d.tdiff; }),
-      time_alloweds = time_allowed.group(),
-      
-      idDimension = flight.dimension(function(p, i) { return i; }),
-	  idGrouping = idDimension.group(function(id) { return id; }),
-      
-   
-    deviation = flight.dimension(function(d) { return d.dev; }),
-    deviations = deviation.group(function(d) { return Math.floor(d / 10) * 10; });
+  hour = flight.dimension(function(d) { return d.TA.getHours(); }),
+  hours = hour.group(function(d) {return Math.floor(d);}),
 
-      
+  cruise_pc = flight.dimension(function(d) { return d.cruise_pc; }),
+  cruise_pcs = cruise_pc.group(function(d) { return Math.floor(d * 10) / 10;}),
 
-  var charts = [
+  FB_pc = flight.dimension(function(d) { return d.FB_pc; }),
+  FB_pcs = FB_pc.group(function(d) { return Math.floor(d * 10) / 10;}),
 
-   //row1
-	barChart() // Time of Day
-	        .dimension(hour)
-	        .group(hours)
-	      .x(d3.scale.linear()
-	        .domain([0, 24])
-	        .rangeRound([0, 240])),
+  FB_kg = flight.dimension(function(d) { return d.FB_tonne; }),
+  FB_kgs = FB_kg.group(function(d) { return Math.floor(d * 25) / 25; }),
 
-	barChart() // Flight day
-	        .dimension(day)
-	        .group(dates)
-	        .round(d3.time.day.round)
-	      .x(d3.time.scale()
-	        .domain([new Date(2014, 08, 1), new Date(2014, 08, 8)])
-	        .rangeRound([0, 10 * 48])),
+  FB_co2 = flight.dimension(function(d) { return d.FB_co2_tonne; }),
+  FB_co2s = FB_co2.group(function(d) { return Math.floor(d * 25) / 25; }),
 
-   //row2
-    barChart() // PC of flight in Cruise
-	        .dimension(cruise_pc)
-	        .group(cruise_pcs)
-	      .x(d3.scale.linear()
-	        .domain([0, 100])
-	        .rangeRound([0, 10 * 24])),
-	       
-    barChart() // Flight Distances
-	        .dimension(distance)
-	        .group(distances)
-	      .x(d3.scale.linear()
-	        .domain([0, (Math.floor(distance.top(1)[0].Distance/500)+2)*500])
-	        .rangeRound([0, 10 * 48])),
-	        
-  //row3
-   barChart() // PC saving in FB
-	        .dimension(FB_pc)
-	        .group(FB_pcs)
-	      .x(d3.scale.linear()
-	        .domain([0, 10])
-	        .rangeRound([0, 10 * 24])),
-	              
-   barChart() // Deviations
-	        .dimension(deviation)
-	        .group(deviations)
-	      .x(d3.scale.linear()
-	        .domain([0, (Math.floor(deviation.top(1)[0].dev/25)+2)*25])
-	        .rangeRound([0, 10 * 24])),
-	        
-   barChart() // time alloweds
-	        .dimension(time_allowed)
-	        .group(time_alloweds)
-	      .x(d3.scale.linear()
-	        .domain([0, (Math.floor(time_allowed.top(1)[0].tdiff/60)+2)*60])
-	        .rangeRound([0, 10 * 24]))
+  distance = flight.dimension(function(d) { return d.Distance; }),
+  distances = distance.group(function(d) { return Math.floor(d * 50) / 50; }),
 
+  time_allowed = flight.dimension(function(d) { return d.tdiff; }),
+  time_alloweds = time_allowed.group(),
+
+  idDimension = flight.dimension(function(p, i) { return i; }),
+  idGrouping = idDimension.group(function(id) { return id; }),
+
+
+deviation = flight.dimension(function(d) { return d.dev; }),
+deviations = deviation.group(function(d) { return Math.floor(d / 10) * 10; });
+
+
+
+var charts = [
+
+//row1
+barChart() // Time of Day
+        .dimension(hour)
+        .group(hours)
+      .x(d3.scale.linear()
+        .domain([0, 24])
+        .rangeRound([0, 240])),
+
+barChart() // Flight day
+        .dimension(day)
+        .group(dates)
+        .round(d3.time.day.round)
+      .x(d3.time.scale()
+        .domain([new Date(2014, 08, 1), new Date(2014, 08, 8)])
+        .rangeRound([0, 10 * 48])),
+
+//row2
+barChart() // PC of flight in Cruise
+        .dimension(cruise_pc)
+        .group(cruise_pcs)
+      .x(d3.scale.linear()
+        .domain([0, 100])
+        .rangeRound([0, 10 * 24])),
+
+barChart() // Flight Distances
+        .dimension(distance)
+        .group(distances)
+      .x(d3.scale.linear()
+        .domain([0, (Math.floor(distance.top(1)[0].Distance/500)+2)*500])
+        .rangeRound([0, 10 * 48])),
+
+//row3
+barChart() // PC saving in FB
+        .dimension(FB_pc)
+        .group(FB_pcs)
+      .x(d3.scale.linear()
+        .domain([0, 10])
+        .rangeRound([0, 10 * 24])),
+
+barChart() // Deviations
+        .dimension(deviation)
+        .group(deviations)
+      .x(d3.scale.linear()
+        .domain([0, (Math.floor(deviation.top(1)[0].dev/25)+2)*25])
+        .rangeRound([0, 10 * 24])),
+
+barChart() // time alloweds
+        .dimension(time_allowed)
+        .group(time_alloweds)
+      .x(d3.scale.linear()
+        .domain([0, (Math.floor(time_allowed.top(1)[0].tdiff/60)+2)*60])
+        .rangeRound([0, 10 * 24])),
+
+//row4
+barChart() // PC saving in FB
+      .dimension(FB_kg)
+      .group(FB_kgs)
+    .x(d3.scale.linear()
+      .domain([0, (Math.floor(FB_kg.top(1)[0].FB_tonne/5)+2)*5])
+      .rangeRound([0, 10 * 36])),
+
+barChart() // Deviations
+      .dimension(FB_co2)
+      .group(FB_co2s)
+    .x(d3.scale.linear()
+      .domain([0, (Math.floor(FB_co2.top(1)[0].FB_co2_tonne/5)+2)*5])
+      .rangeRound([0, 10 * 36]))
 
 
 /* 	        .filter([new Date(2001, 1, 1), new Date(2001, 2, 1)]) */
 
+];
 
-  ];
-  
-   // bind charts to dom
-  domCharts = d3.selectAll(".chart")
-      .data(charts)
-      .each(function(chart) { chart.on("brush", renderAll).on("brushend", renderAll); });
+// bind charts to dom
+domCharts = d3.selectAll(".chart")
+  .data(charts)
+  .each(function(chart) { chart.on("brush", renderAll).on("brushend", renderAll); });
 
-  
-	// Renders all of the charts
-	function updateCharts() {
-	  domCharts.each(render);
-	}
+
+// Renders all of the charts
+function updateCharts() {
+  domCharts.each(render);
+}
 
 
 
@@ -208,11 +254,11 @@ function initMap() {
   google.maps.visualRefresh = true;
   colorgrad = ["#EDD52C",'#D5D82B','#BEDC2B','#A7E02B','#8FE42B','#78E82A','#61EC2A','#49F02A','#32F42A','#1BF82A'];
   var myLatlng = new google.maps.LatLng(48.6, 8.5);
-  
+
   style1=[{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];
-  
+
   style2=[{"featureType":"water","stylers":[{"color":"#46bcec"},{"visibility":"on"}]},{"featureType":"landscape","stylers":[{"color":"#f2f2f2"}]},{"featureType":"road","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]}];
-  
+
   style3=[{"featureType":"water","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#e9ebed"},{"saturation":-90},{"lightness":-8},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":10},{"lightness":69},{"visibility":"on"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"hue":"#2c2e33"},{"saturation":7},{"lightness":19},{"visibility":"on"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]}];
   var mapOptions = {
     zoom: 3,
@@ -224,10 +270,10 @@ function initMap() {
     styles: style2
   };
   map = new google.maps.Map(document.getElementById('map-div'), mapOptions);
-  
+
   for (var i = 0; i < flights.length; i++) {
 	  var point = flights[i];
-	 
+
  var flightPlanCoordinates = [
  	    new google.maps.LatLng(point.lat1, point.lon1),
 	    new google.maps.LatLng(point.lat3, point.lon3),
@@ -315,9 +361,9 @@ function updateMarkers() {
   var total_FB_solo = flight.groupAll().reduceSum(function(d) { return d.FB_solo;}).value()
   var total_FB_form = flight.groupAll().reduceSum(function(d) { return d.FB_form;}).value()
   avg_pc = (1-(total_FB_form/total_FB_solo));
-  
-  
-  
+
+
+
   // Render the total.
   d3.selectAll("#total")
       .text(formatNumber(flight.size()));
@@ -327,7 +373,7 @@ function updateMarkers() {
       .text( formatPC(avg_pc) );
 
   renderAll();
-	 
+
 
 
   // Renders the specified chart or list.
@@ -340,8 +386,8 @@ function updateMarkers() {
     chart.each(render);
     list.each(render);
     d3.select("#active").text(formatNumber(all.value()));
-    
-   	
+
+
 	//avg pc
 	var total_FB_solo = flight.groupAll().reduceSum(function(d) { return d.FB_solo;}).value()
   	var total_FB_form = flight.groupAll().reduceSum(function(d) { return d.FB_form;}).value()
@@ -357,17 +403,28 @@ function updateMarkers() {
     d3.select("#avg_dev_active").text(formatNo(avg_dev)); //render the pc saving
     //avg cruise_pc
     avg_cruise_pc = flight.groupAll().reduceSum(function(d) { return d.cruise_pc;}).value()/(100*all.value());
-    d3.select("#avg_cruise_pc_active").text(formatPC(avg_cruise_pc)); //render the pc saving    
+    d3.select("#avg_cruise_pc_active").text(formatPC(avg_cruise_pc)); //render the pc saving
    //avg tdiff
     avg_tdiff = flight.groupAll().reduceSum(function(d) { return d.tdiff;}).value()/all.value();
     d3.select("#avg_tdiff_active").text(formatNo(avg_tdiff)); //render the pc saving
 
+    total_fb_kb = flight.groupAll().reduceSum(function(d) { return d.FB_tonne;}).value();
+    d3.select("#fb-kg-total").text(formatNo(total_fb_kb)); //render the pc saving
+
+    total_fb_co2 = flight.groupAll().reduceSum(function(d) { return d.FB_co2_tonne;}).value();
+    d3.select("#co2-kg-total").text(formatNo(total_fb_co2)); //render the pc saving
+
+    d3.select("#active-forms").text(formatNo(all.value()));
+
+
 /*      initMap(); */
+if (plotmap == true){
+    console.log('updating markers and charts')
     updateMarkers();
     updateCharts();
 /*      dc.renderAll(); */
+}
 
-    
   }
 
   // Like d3.time.format, but faster.
@@ -385,23 +442,23 @@ function updateMarkers() {
         dt.substring(0, 2),
         dt.substring(2, 4));
   }
-  
+
   function parseTname(dt) {
     var daynum = ['15','30','45','60','120','240'].indexOf(dt) +1;
     return daynum;
   }
-  
+
 
   window.filter = function(filters) {
     filters.forEach(function(d, i) { charts[i].filter(d); });
     renderAll();
   };
-  
+
   window.filterIATA = function(filters) {
     filters.forEach(function(d, i) { flight[i].filter(d); });
     renderAll();
   };
-  
+
 
   window.reset = function(i) {
     charts[i].filter(null);
@@ -418,9 +475,9 @@ function updateMarkers() {
       date.enter().append("div")
           .attr("class", "date")
         .append("div")
-          .attr("class", "day")
+          .attr("class", "none")
           .text(function(d) { return d.FB_pc; });
-
+          
       date.exit().remove();
 
       var flight = date.order().selectAll(".flight")
@@ -429,7 +486,7 @@ function updateMarkers() {
       var flightEnter = flight.enter().append("div")
           .attr("class", "flight");
     /*
-      
+
       flightEnter.append("div")
 	      .attr("class", "time")
 	      .text(function(d) { return d.day; });
@@ -438,22 +495,22 @@ function updateMarkers() {
 	  flightEnter.append("div")
           .attr("class", "day")
           .text(function(d) { return d.day ; });
-          	        
+
 
 
       flightEnter.append("div")
           .attr("class", "origin")
           .text(function(d) { return d.Dept_A + " - " + d.Dest_A; });
-          
-        
+
+
       flightEnter.append("div")
           .attr("class", "time")
-          .text(function(d) { return formatTime(d.TA) ; });  
+          .text(function(d) { return formatTime(d.TA) ; });
 
       flightEnter.append("div")
           .attr("class", "destination")
           .text(function(d) { return d.Dept_B + " - " + d.Dest_B; });
-          
+
       flightEnter.append("div")
           .attr("class", "time")
           .text(function(d) { return formatTime(d.TB) ; });
@@ -463,21 +520,29 @@ function updateMarkers() {
           .text(function(d) { return (Math.floor(d.FB_pc*100)/100) + "%"; });
 
       flightEnter.append("div")
+          .attr("class", "fb-saving")
+          .text(function(d) { return (Math.floor(d.FB_tonne*100)/100); });
+
+      flightEnter.append("div")
+          .attr("class", "co2-saving")
+          .text(function(d) { return (Math.floor(d.FB_co2_tonne*100)/100); });
+
+      flightEnter.append("div")
           .attr("class", "cruise")
           .text(function(d) { return (Math.floor((d.cruise_pc*10)/10)) +"%"; });
 
-	  
+
      flightEnter.append("div")
-	      .attr("class", "cruise")
-	      .text(function(d) { return (Math.floor(d.tdiff)) +" m"; });	
-		
+	      .attr("class", "tdiff")
+	      .text(function(d) { return (Math.floor(d.tdiff)) +" m"; });
+
 
       flight.exit().remove();
 
       flight.order();
     });
   }
-  
+
 
   function barChart() {
     if (!barChart.id) barChart.id = 0;
